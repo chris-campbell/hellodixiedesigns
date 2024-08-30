95b8bc08d9f33b84fcd5410fdc037237d3a281ba
ccampbell1904 <hopprdev@gmail.com>
2024-08-30 13:09:24 -0400

    Restore

--- wp-content/themes/hellodixiedesigns/partials/blog-single/headline.php
+++ wp-content/themes/hellodixiedesigns/partials/blog-single/headline.php
@@ -12,9 +12,9 @@ $author = $args['author'];
         <p class="mb-6 font-aglet-sans font-normal text-dixie-mint text-xs leading-5 max-w-140 font">
             <?php echo $excerpt; ?>
         </p>
-        <!-- <div class="flex justify-between md:pb-16 pb-8">
+        <div class="flex justify-between md:pb-16 pb-8">
             <p class="font-aglet-sans text-xs text-dixie-mint"><?php echo $date; ?></p>
             <p class="font-aglet-sans text-xs text-dixie-mint"><?php echo $author; ?></p>
-        </div> -->
+        </div>
     </div>
 </section>
