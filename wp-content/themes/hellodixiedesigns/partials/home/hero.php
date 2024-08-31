c6c5cd9bd35a9941600bbe247390cbca987478fd
ccampbell1904 <hopprdev@gmail.com>
2024-08-31 08:17:03 -0400

    Initial

--- wp-content/themes/hellodixiedesigns/partials/home/hero.php
+++ wp-content/themes/hellodixiedesigns/partials/home/hero.php
@@ -7,9 +7,15 @@ $desktop_logo = get_stylesheet_directory_uri() . '/frontend/images/typing.png';
         <div class="max-w-244 px-8 py-8 md:p-16">
             <p class="font-aglet-sans font-semibold text-dixie-royal mb-2 max-w-148 text-xs">Feature Article</p>
             <h1 class="font-chivo-bold pb-6 md-2:text-4xl text-lg-2 capitalize">
-                <?php the_field('featured_brand_title'); ?>
+                <?php 
+                the_field('featured_brand_title'); 
+                ?>
             </h1>
-            <p class="font-aglet-sans pb-6 max-w-148 text-xs"><?php the_field('featured_article_description'); ?></p>
+            <p class="font-aglet-sans pb-6 max-w-148 text-xs">
+                <?php 
+                the_field('featured_article_description'); 
+                ?>
+            </p>
 
             <a href="<?php the_field('feature_button_link'); ?>">
                 <button
